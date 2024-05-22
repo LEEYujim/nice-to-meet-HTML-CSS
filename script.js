2ae8705b21ef148c75c9adf2767189bcafac413b
YUJ <164837284+LEEYujim@users.noreply.github.com>
2024-05-22 18:17:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-$(function(){
+$('document').ready(function(){
 
-    let currentIndex = 0;
-    $(".slider").hide().first().show();
-
-    setInterval(function(){
-        let nextIndex = (currentIndex+1)%3;
-
-        $(".slider").eq(currentIndex).fadeOut(1200);
-        $(".slider").eq(nextIndex).fadeIn(1200);
-
-        currentIndex=nextIndex;
-    },3000);
-});
-
-$(".nav>ul>li").mouseover(function(){
-    $("this").find(".submenu").stop().slideDown(200);
-});
-$(".nav>ul>li").mouseout(function(){
-    $("this").find(".submenu").stop().slideUp(200);
-});
-const tabBtn = $(".info-menu>a");
-const tabCont = $(".info-cont>div");
-tabCont.hide().eq(O).show();
-
-tabBtn.on("click, function(){
-    const index = $(this).index();
-
-    $(this).addClass("active").siblings().removeClass("active");
-    tabCont.eq(index).show().siblings().hide();
-});
-
-$(".popup-btn").click(function(){
-    $(".popup-view").show();
-});
-$(".popup-close").click(function(){
-    $(".popup-view").hide();
+    $('.pp').click(function(){
+        $('.popup').show();
+    });
+    $('.button button').click(function(){
+        $('.popup').hide();
+    });
 });
