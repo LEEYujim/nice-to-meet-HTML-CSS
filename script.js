a2a9bcd3f404beaa9f02d12cbda4c177b689c0d6
YUJ <164837284+LEEYujim@users.noreply.github.com>
2024-05-27 21:38:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,29 @@ $(document).ready(function(){
 
     //slide
     function slide(){
-        $('.slide ul').animate({top:"-300px"},1000,
-        function(){$('.slide ul').append($('.slide ul li').first());
-            $('.slide ul').css({top:"0"});
-        });
+        $('.slide ul').animate({right: "-100%"},1000,
+            function(){
+                $('.slide ul').append($('.slide ul li').first());
+                $('.slide ul').css({right:"0px"});
+            });
     }
-    setInterval(slide,3000)
+    
+    setInterval(slide,3000);
 
-    //nav
+    //menu
     $('nav>ul>li').mouseenter(function(){
-        $(this).find(".sub").stop().slideDown();
+        $('.sub, .menubg').stop().slideDown();
     });
     $('nav>ul>li').mouseleave(function(){
-        $(".sub").stop().slideUp();
+        $('.sub, .menubg').stop().slideUp();
     });
+    
 
     //popup
     $('.pp').click(function(){
         $('.popup').show();
     });
-    $('.btn button').click(function(){
+    $('button').click(function(){
         $('.popup').hide();
     });
 
